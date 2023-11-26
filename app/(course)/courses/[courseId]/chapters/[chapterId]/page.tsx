1e8b236935e9bcb8b9976d8446b3fff04cb47caa
Haseeb4545 <152041736+Haseeb4545@users.noreply.github.com>
2023-11-26 07:42:10 +0000

    hi

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -4,6 +4,9 @@ import {auth} from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { VideoPlayer } from "./_components/video-player";
 import { CourseEnrollButton } from "./_components/course-enrollbutton";
+import { Separator } from "@/components/ui/separator";
+import { Preview } from "@/components/preview";
+import { File } from "lucide-react";
  
 
 const ChapterIdPage = async({params}:{params:
@@ -77,6 +80,31 @@ price={course?.price!}
 />
      )}
         </div>
+        <Separator/>
+        <div>
+            <Preview
+            value={chapter?.description!}/>
+        </div>
+        {!!attachments.length && (
+            <>
+            <Separator/>
+            <div className="p-4">
+{attachments.map((attachment)=>(
+    <a href={attachment?.url}
+    key={attachment?.id}
+    target="_blank"
+    className="flex items-center p-3 w-full bg-sky-200 border
+    text-sky-700
+    rounded-md hover:underline">
+        <File/>
+        <p className="line-clamp-1">
+            {attachment.name}
+        </p>
+    </a>
+))}
+            </div>
+            </>
+        )}
     </div>
     </div>       
         </div> );
